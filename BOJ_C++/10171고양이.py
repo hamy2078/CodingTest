1c1138fa582a6cb127edff61205e118e99fa1a2f
hamy2078 <63527698+hamy2078@users.noreply.github.com>
2022-03-03 12:27:05 +0900

    Create 10171고양이.py

---
+++ BOJ_C++/10171고양이.py
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  // 특수 문자 출력하기 연습
+	cout << "\\    /\\" << endl;
+	cout << " )  ( ')" << endl;
+	cout << "(  /  )" << endl;
+	cout << " \\(__)|" << endl;
+	return 0;
+};
