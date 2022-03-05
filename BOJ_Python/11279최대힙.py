1e1f0c952e4d4b5abac4a3c4f4c3ce0433a9d5b4
hamy2078 <63527698+hamy2078@users.noreply.github.com>
2022-03-05 16:10:55 +0900

    Create 11279최대힙.py

---
+++ BOJ_Python/11279최대힙.py
@@ -0,0 +1,15 @@
+import sys
+import heapq
+
+min_q = []
+n = int(sys.stdin.readline())
+for _ in range(n):
+    x = int(sys.stdin.readline())
+    if x == 0:
+        try:
+            sys.stdout.write(str(-heapq.heappop(min_q))+'\n')
+        except:
+            sys.stdout.write(str(0)+'\n')
+    else:
+        heapq.heappush(min_q,-x)
+        
