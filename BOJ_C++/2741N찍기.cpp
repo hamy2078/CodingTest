5aac592f0b31aad7d730732b3ecbb4e60214295c
hamy2078 <63527698+hamy2078@users.noreply.github.com>
2022-03-05 16:32:47 +0900

    Create 2741N찍기.cpp

---
+++ BOJ_C++/2741N찍기.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		// std::endl은 단순히 개행만 하는 것이 아닌 버퍼를 비우는 작업도 같이 하기 때문에
+		// "\n"을 사용하는 것이 시간적인 면에서는 좋다.
+		cout << i << "\n";
+	}
+	return 0;
+};
